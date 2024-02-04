dc4c66c656981a83799063bba23449bdb744d98c
liannedeldacan <liannefrancheskadeldacan@gmail.com>
2024-02-05 07:30:57 +0800

    updated

---
+++ Student/StudentLayout/sidebarGrades.php
@@ -0,0 +1,51 @@
+ <aside>
+<?php
+require('../db/config.php');
+
+?>
+
+   <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active">
+            <a class="nav-link" href="studentGrades.php">
+                <span style="font-size: 20px;">1ST QUARTER</span>
+            </a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <li class="nav-item active">
+            <a class="nav-link" href="studentGrades.php?quarter='2'">
+                <span style="font-size: 20px;">2ND QUARTER</span>
+            </a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <li class="nav-item active">
+            <a class="nav-link" href="studentGrades.php?quarter='3'">
+                <span style="font-size: 20px;">3RD QUARTER</span>
+            </a>
+
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+
+        <li class="nav-item active">
+            <a class="nav-link" href="studentGrades.php?quarter='4'">
+                <span style="font-size: 20px;">4TH QUARTER</span>
+            </a>
+        </li>
+        
+    </ul>
+    <!-- End of Sidebar -->
+    
+</aside>
